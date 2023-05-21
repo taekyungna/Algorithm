655a9c5ae36288bd629232038edf493925c5d31c
나태경 <121482720+taekyungna@users.noreply.github.com>
2023-05-21 13:51:30 +0900

    [Bronze V] Title: ??!, Time: 208 ms, Memory: 17740 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main{
+    
+    public static void main(String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String a = sc.nextLine();
+        
+        String b = a;
+        
+        
+        if (a==b){
+            System.out.println(a+"??!");
+        }
+        
+    }
+    
+}  
