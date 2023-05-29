0a436841c090cc63b7b6c0d8a36c66335767c6db
나태경 <121482720+taekyungna@users.noreply.github.com>
2023-05-29 20:30:54 +0900

    [Bronze V] Title: 두 수 비교하기, Time: 220 ms, Memory: 17948 KB -BaekjoonHub

---
+++ 백준/Bronze/1330. 두 수 비교하기/두 수 비교하기.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main{
+    
+    public static void main(String [] args){
+        
+        Scanner sc = new Scanner(System.in);
+        
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        
+        if (a>b){
+            
+            System.out.println(">");
+            
+        }else if(a<b){
+            
+            System.out.println("<");
+            
+        }else if(a==b){
+            
+            System.out.println("==");
+        }
+        
+        
+    }
+    
+    
+}
