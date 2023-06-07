dea45947c4afd9ba55f0bee28a92a5140982a3f7
나태경 <121482720+taekyungna@users.noreply.github.com>
2023-06-07 12:25:36 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class Main{
+public static void main(String [] args){
+    
+		
+		 Scanner sc = new Scanner(System.in);
+	        
+	        int a = sc.nextInt();
+	        int num = 0;
+	        
+	            for(int i=0; i<=a; i++){
+	            	
+	            		num += i;
+	            				
+	            				
+	            }
+	 
+	            System.out.print(num);
+	        
+	    }
+	    
+	    
+	}	    
