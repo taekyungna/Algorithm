f20daeb6be9c59c98f0ae8cd139c79472b5428d6
나태경 <121482720+taekyungna@users.noreply.github.com>
2023-06-13 13:40:32 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 308 ms, Memory: 18060 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Main{
+    
+    public static void main(String [] args){
+        
+     Scanner sc = new Scanner(System.in);
+	        
+	        int num = sc.nextInt();
+	        
+	        for(int i = 0; i<num; i++){
+	            
+	            for(int k=1; k<=i; k++){
+	                
+	                
+	            	System.out.print("*");
+	            }
+	            
+	            System.out.println("*");
+	            
+	        }
+	        
+	        
+	    }
+	    
+	    
+	}
